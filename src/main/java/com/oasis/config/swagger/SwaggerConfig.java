b26b6bfa75611585b965cd8338795e9f2fa2e763
조대준 <eowns36@gmail.com>
2022-09-07 23:04:27 +0900

    swagger basePackage 수정
    basePackage 를 com.oasis로 지정하여 불필요한
     basic error controller 표시되지 않도록 수정

--- src/main/java/com/oasis/config/swagger/SwaggerConfig.java
+++ src/main/java/com/oasis/config/swagger/SwaggerConfig.java
@@ -23,7 +23,7 @@ public class SwaggerConfig {
                 .useDefaultResponseMessages(true) // Swagger 에서 제공해주는 기본 응답 코드 (200, 401, 403, 404) 등의 노출 여부
                 .apiInfo(apiInfo()) // Swagger UI 로 노출할 정보
                 .select()
-                .apis(RequestHandlerSelectors.any()) // api 스펙이 작성되어 있는 패키지 (Controller) 지정
+                .apis(RequestHandlerSelectors.basePackage("com.oasis")) // api 스펙이 작성되어 있는 패키지 (Controller) 지정
                 .paths(PathSelectors.any()) // apis 에 위치하는 API 중 특정 path 를 선택
                 .build();
     }
