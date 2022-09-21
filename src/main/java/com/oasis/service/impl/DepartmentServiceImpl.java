0cd58efa829a46e71f43d9a79c04396f7c2c6f21
jwmoon <mjw3000kr@naver.com>
2022-09-21 23:33:58 +0900

    1. 부서를 찾을 수 없을때 exception 발생하게 변경

--- src/main/java/com/oasis/service/impl/DepartmentServiceImpl.java
+++ src/main/java/com/oasis/service/impl/DepartmentServiceImpl.java
@@ -1,5 +1,7 @@
 package com.oasis.service.impl;
 
+import com.oasis.common.constant.ErrorCode;
+import com.oasis.common.exception.CommonException;
 import com.oasis.data.dto.request.DepartmentRequestDto;
 import com.oasis.data.dto.response.DepartmentResponseDto;
 import com.oasis.data.entity.Department;
@@ -8,7 +10,9 @@ import com.oasis.service.DepartmentService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.annotation.PostConstruct;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -19,6 +23,18 @@ public class DepartmentServiceImpl implements DepartmentService {
 
     private final DepartmentRepository departmentRepository;
 
+    /*
+    @PostConstruct
+    public void init() {
+        Department department = Department.builder()
+                .name("/")
+                .level(0)
+                .parentSid(0L)
+                .build();
+        departmentRepository.save(department);
+    }
+    */
+
     public List<DepartmentResponseDto> list() {
         return departmentRepository.findAll().stream()
                 .map(d -> new DepartmentResponseDto(d.getSid(), d.getParentSid(), d.getName(), d.getLevel()))
@@ -27,7 +43,7 @@ public class DepartmentServiceImpl implements DepartmentService {
 
     @Transactional
     public void save(DepartmentRequestDto dto){
-        Department parent = departmentRepository.findById(dto.getParentSid()).orElseThrow(RuntimeException::new);
+        Department parent = departmentRepository.findById(dto.getParentSid()).orElseThrow(()-> new CommonException(ErrorCode.NOT_FOUND_DEPARTMENT));
         Department department = Department.builder()
                 .name(dto.getName())
                 .parentSid(dto.getParentSid())
@@ -37,8 +53,8 @@ public class DepartmentServiceImpl implements DepartmentService {
     }
     @Transactional
     public void update(Long sid, DepartmentRequestDto dto) {
-        Department current = departmentRepository.findById(sid).orElseThrow(RuntimeException::new);
-        Department parent = departmentRepository.findById(dto.getParentSid()).orElseThrow(RuntimeException::new);
+        Department current = departmentRepository.findById(sid).orElseThrow(()-> new CommonException(ErrorCode.NOT_FOUND_DEPARTMENT));
+        Department parent = departmentRepository.findById(dto.getParentSid()).orElseThrow(()-> new CommonException(ErrorCode.NOT_FOUND_DEPARTMENT));
         if(current.getParentSid() == dto.getParentSid() && current.getName().equals(dto.getName())) {
             return;
         }
@@ -61,6 +77,6 @@ public class DepartmentServiceImpl implements DepartmentService {
     public DepartmentResponseDto detail(Long sid) {
         return departmentRepository.findById(sid)
                 .map(d -> new DepartmentResponseDto(d.getSid(), d.getParentSid(), d.getName(), d.getLevel()))
-                .orElseThrow(RuntimeException::new);
+                .orElseThrow(()-> new CommonException(ErrorCode.NOT_FOUND_DEPARTMENT));
     }
 }
