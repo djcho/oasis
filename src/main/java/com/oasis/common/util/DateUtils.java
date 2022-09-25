398c0b64612c2f9a7e63b8410ee6373222a95256
jwmoon <mjw3000kr@naver.com>
2022-09-26 00:23:13 +0900

    1. 기본적인 dateutil 생성 2. dateutil 테스트 작성

---
+++ src/main/java/com/oasis/common/util/DateUtils.java
@@ -0,0 +1,33 @@
+package com.oasis.common.util;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.time.Period;
+
+public class DateUtils {
+
+    public static LocalDate getFirstDayOfMonth(LocalDate date) {
+        return date.withDayOfMonth(1);
+    }
+    public static LocalDate getLastDayOfMonth(LocalDate date) {
+        return date.withDayOfMonth(date.lengthOfMonth());
+    }
+
+    public static int remainDaysOfMonth(LocalDate start, LocalDate end) {
+        return Period.between(start,end).getDays();
+    }
+
+    public static int countByWorkingDay(LocalDate date) {
+        LocalDate start = getFirstDayOfMonth(date);
+        LocalDate end = getLastDayOfMonth(date);
+
+        long ret = start.datesUntil(end).filter(d -> isWorkingDay(d)).count();
+
+        return (int)ret;
+    }
+
+    public static boolean isWorkingDay(LocalDate date) {
+        DayOfWeek dow = date.getDayOfWeek();
+        return (dow != DayOfWeek.SATURDAY && dow != DayOfWeek.SUNDAY);
+    }
+}
---
+++ src/test/java/com/oasis/DateUtilsTest.java
@@ -0,0 +1,27 @@
+package com.oasis;
+
+import com.oasis.common.util.DateUtils;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DateUtilsTest {
+
+    @Test
+    public void 날짜계산_test() {
+        LocalDate date = LocalDate.now();
+
+        LocalDate start = DateUtils.getFirstDayOfMonth(date);
+        LocalDate end = DateUtils.getLastDayOfMonth(date);
+
+        int remainDay = DateUtils.remainDaysOfMonth(start, end);
+
+        assertThat(remainDay + 1).isEqualTo(date.lengthOfMonth());
+
+        System.out.println(DateUtils.countByWorkingDay(date));
+    }
+
+}
